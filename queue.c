17ae6482e433017a75c9dca5fc2d69bc759a28d6
disastrpc <jaredfreed@protonmail.com>
2021-05-08 01:05:45 -0400

    implemented sjf and ps

--- queue.c
+++ queue.c
@@ -66,99 +66,11 @@ void pop(node **p_head, task *p_task) {
     *p_head = n_node;
 }
 
-// Places task at the end of queue, skipping it
-void skip(node *p_head, task *p_task) {
-
-    node *i = p_head;
-
-    // Iterate through list until the end is reached
-    while(i->n != NULL) {
-        i = i->n;
-    }
-
-    node *n_node = (node*) malloc(sizeof(node));
-
-    n_node->data.id = p_task->id;
-    n_node->data.weight = p_task->weight;
-    n_node->data.burst = p_task->burst;
-    n_node->n->n = NULL;
-    
-}
-
-// Separates item from the end of queue in typical FIFO fashion
-// Takes pointer to the list head and a task struct pointer to modify
-void separate(node *p_head) {
-    
-    // Check if list contains only one item
-    if(p_head->n == NULL) {
-
-        // Assign values and free current head's memory
-        // p_task->id = p_head->data.id;
-        // p_task->weight = p_head->data.weight;
-        // p_task->burst = p_head->data.burst;
-        free(p_head);
-
-        return;
-    }
-
-    // Otherwise we must look forward two indexes in the list
-    // in order to change the last item and the one before
-    // This moves our pointer to the item before the last index
-    node *i = p_head;
-    while(i->n->n != NULL) {
-        i = i->n;
-    }
-
-    // Now we can use the pointer to access the next spot 
-    // on the list to assign data, free memory, and set the
-    // next variable to NULL on the current node
-    // p_task->id = i->n->data.id;
-    // p_task->weight = i->n->data.weight;
-    // p_task->burst = i->n->data.burst;
-    free(i->n);
-    i->n = NULL;
-}
-
-void pull(node **p_head, task *p_task, unsigned int index) {
-
-    node *i = *p_head;
-    node *t_node = NULL;
-
-    // To avoid iterating over the list simply invoke pop() if index if 0
-    if(index == 0) {
-        pop(p_head, NULL);
-    }
-
-    // Iterate pointer to spot before specified index 
-    for(int n = 0; n < index - 1; n++) {
-
-        // Check if next is NULL, if it is something went wrong
-        if(i->n == NULL) {
-            perror("[-] ERROR: Invalid index");
-            return;
-        }
-
-        // Grab value from next to advance pointer
-        i = i->n;
-    }
-
-    // Store the last index in a temprary node
-    t_node = i->n;
-
-    // Assign data to provided struct from temporary node
-    p_task->id = t_node->data.id;
-    p_task->weight = t_node->data.weight;
-    p_task->burst = t_node->data.burst;
-
-    // Overwrite with temporary node
-    i->n = t_node->n;
-
-    free(t_node);
-}
-
 void swap(node *n1, node *n2) {
-    task *tmp = &n1->data;
+    
+    node tmp;
+    tmp.data = n1->data;
     n1->data = n2->data;
-    n2->data = *tmp;
+    n2->data = tmp.data;
 
 }
--- scheduler.c
+++ scheduler.c
@@ -6,7 +6,7 @@
 #include "cpu.h"
 #include "scheduler.h"
 
-/* Accepts pointer to task and stats strucs as well as the size of the queue
+/* Accepts pointer to task and stats structs as well as the size of the queue
    Modifies the stats struct to the average turnaround, wait and reponse values */ 
 void gen_stats(task *t, stats *s, int size) {
 
@@ -49,39 +49,118 @@ void *fcfs(node *head) {
 
 void *sjf(node *head) {
 
+    // Start node
     node *i = head;
+    // Used for walking the list
+    node *j;
+    // Represents min value
+    node *m;
     task *t = malloc(sizeof(task));
     stats *s = malloc(sizeof(stats));
-    node *j;
-    node *m;
-    int k;
 
+    // Walks lists and sorts according to the CPU burst
     while(i->n) {
         m = i;
         j = i->n;
-        while(t) {
+
+        // Inner loop compares minimum element with element from next node
+        while(j) {
             if(m->data.burst > j->data.burst) {
                 m = j;
             }
 
             j = j->n;
         }
-
+        
         swap(i, m);
         i = i->n;
+    }
 
+
+    int c, k;
+    c = 0;
+    k = 0;
+    i = head;
+
+    while(i != NULL) {
+
+        if(i->n == NULL) {
+            i->data.wait = (c - i->data.burst) + k;
+        } else {
+            i->data.wait = c + k;
+        }
+
+        i->data.resp += c - k;
+        c += i->data.burst;
+    
+        i->data.turn = c;
+        k++;
         process(i);
         pop(&i, t);
-
-        k++;
     }
 
     gen_stats(t, s, k);
+
     return s;
 }
-// int get_avg_wait(int times[], int size) {
 
-// }
+void *ps(node *head) {
+
+    // Start node
+    node *i = head;
+    // Used for walking the list
+    node *j;
+    // Represents min value
+    node *m;
+    task *t = malloc(sizeof(task));
+    stats *s = malloc(sizeof(stats));
+
+    // Walks lists and sorts according to the CPU burst
+    while(i->n) {
+        m = i;
+        j = i->n;
+
+        // Inner loop compares minimum element with element from next node
+        while(j) {
+            if(m->data.weight > j->data.weight) {
+                m = j;
+            }
+
+            j = j->n;
+        }
+        
+        swap(i, m);
+        i = i->n;
+    }
+
+
+    int c, k;
+    c = 0;
+    k = 0;
+    i = head;
+
+    while(i != NULL) {
+
+        if(i->n == NULL) {
+            i->data.wait = (c - i->data.burst) + k;
+        } else {
+            i->data.wait = c + k;
+        }
+
+        i->data.resp += c - k;
+        c += i->data.burst;
+    
+        i->data.turn = c;
+        k++;
+        process(i);
+        pop(&i, t);
+    }
+
+    gen_stats(t, s, k);
+
+    return s;
+}
+
 void *schedule(node *head, char a[]) {
 
     stats *s;
@@ -89,6 +168,9 @@ void *schedule(node *head, char a[]) {
         s = fcfs(head);
     } else if(strcmp(a, "sjf") == 0) {
         s = sjf(head);
+    } else if(strcmp(a, "ps") == 0) {
+        s = ps(head);
     }
+
     return s;
 }
